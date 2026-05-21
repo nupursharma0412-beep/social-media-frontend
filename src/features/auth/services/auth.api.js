b9ae97d8b8d08dc5fa3982d94b0ee79229ff5388
Jay Singh <jaysinghrajput265@gmail.com>
2026-05-21 15:58:28 +0530

    Fixed all API routes

--- src/features/auth/services/auth.api.js
+++ src/features/auth/services/auth.api.js
@@ -4,15 +4,12 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-
 export default api;
-
 export async function login(username, password) {
   const response = await api.post("/api/auth/login", {
     username,
     password,
   });
-
   return response.data;
 }
 
@@ -22,12 +19,10 @@ export async function register(username, email, password) {
     email,
     password,
   });
-
   return response.data;
 }
 
 export async function getMe() {
   const response = await api.get("/api/auth/get-me");
-
   return response.data;
 }
