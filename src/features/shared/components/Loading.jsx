292373147bfd4474224e14d0d3effc4c6b815545
Jay Singh <jaysinghrajput265@gmail.com>
2026-06-11 09:21:08 +0530

    fix icon imports

--- src/features/shared/components/Loading.jsx
+++ src/features/shared/components/Loading.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import "../loading.scss";
+import styled from 'styled-components';
 
 const Loading = () => {
   return (
-    <div className="loading-container">
-      <div className="loader"></div>
-    </div>
+   <StyledWrapper>
+      <div className="loader" />
+    </StyledWrapper>
   );
 };
+
+const StyledWrapper = styled.div`
+  .loader {
+    width: 5.4em;
+    height: 5.4em;
+    border: 0.4em solid rgb(84, 84, 84);
+    border-left-color: #a23ffc;
+    border-radius: 45%;
+    animation: spin 0.7s linear infinite;
+  }
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }`;
 
 export default Loading;
